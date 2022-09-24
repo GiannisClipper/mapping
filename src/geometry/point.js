fa8bcf750a10e933e0e5bb61e6f59d28929b9a50
giannisclipper <strunstrun@gmail.com>
2022-09-24 14:33:08 +0300

    Update marker icon alignment in 'geometry/point'.

--- src/geometry/point.js
+++ src/geometry/point.js
@@ -9,7 +9,13 @@ const createIcon = ( { color, size } ) => {
     const svg = SVG.MARKER_FILL.replace( "currentColor", color );
     const iconUrl = 'data:image/svg+xml, ' + svg;
     const iconSize = new L.Point( size, size );
-    const icon = new L.Icon( { iconUrl, iconSize } );
+    const iconAnchor = [ size / 2, size ]; // according to topleft icon pixel
+    const popupAnchor = [ 0, -size ]; // according to iconAnchor
+    // const shadowUrl = `data:image/svg+xml, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/></svg>`;
+    // const shadowSize = [ size * 1.2, size * 1.2 ];
+    // const shadowAnchor = [ size * 1.2 / 2, size * 1.2 ]; // according to topleft icon pixel
+    // const icon = new L.Icon( { iconUrl, iconSize, iconAnchor, popupAnchor, shadowUrl, shadowSize, shadowAnchor } );
+    const icon = new L.Icon( { iconUrl, iconSize, iconAnchor, popupAnchor } );
     return icon;
 }
 
